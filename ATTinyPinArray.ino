175541027efebfa95649a1f63bdb7544d89e85fe
Zeke Koch <zeke@akrasia.org>
2014-08-31 11:52:49 -0700

    removed dead code

--- ATTinyPinArray.ino
+++ ATTinyPinArray.ino
@@ -1,100 +1,61 @@
-/*
-  Software serial multple serial test
- 
- Receives from the hardware serial, sends to software serial.
- Receives from software serial, sends to hardware serial.
- 
- The circuit: 
- * RX is digital pin 10 (connect to TX of other device)
- * TX is digital pin 11 (connect to RX of other device)
- 
- Note:
- Not all pins on the Mega and Mega 2560 support change interrupts, 
- so only the following can be used for RX: 
- 10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
- 
- Not all pins on the Leonardo support change interrupts, 
- so only the following can be used for RX: 
- 8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
- 
- created back in the mists of time
- modified 25 May 2012
- by Tom Igoe
- based on Mikal Hart's example
- 
- This example code is in the public domain.
- 
- */
 #include "FastLED.h"
 #include <SoftwareSerial.h>
-#define ledPin 2
+#include <Stepper.h>
+
 #define dataPin 1
-#define otherLed 0
+#define stepPinA 2
+#define stepPinB 0
+#define txPin 3
+#define rxPin 4
 
 #define ledCount 13
+#define STEPS 300
 
-SoftwareSerial mySerial(4, 3); // RX, TX
+Stepper stepper(STEPS, stepPinA, stepPinB);
+
+SoftwareSerial mySerial(rxPin, txPin); // RX, TX
 CRGB leds[ledCount];
 
 void setup()  
 {
   FastLED.addLeds<NEOPIXEL, dataPin>(leds, ledCount);
 
+  pinMode(2, OUTPUT);
+  pinMode(0, OUTPUT);
+
   // set the data rate for the SoftwareSerial port
   mySerial.begin(9600);
-  pinMode(ledPin, OUTPUT);
-  pinMode(otherLed, OUTPUT);
+  stepper.setSpeed(120);
+  
+  // reset to the bottom
+  stepper.step(STEPS);
 }
 
 void loop() // run over and over
 {
-  cylon();
-  myblink(otherLed, 2);
-  delay(1000);
+  // check to see if there are any instructions
   if (mySerial.available())
   {
-    myblink(ledPin, 2);
-
+    // get the instruction and set it into r
     byte r = mySerial.read();
-    myblink(ledPin, r);
-    mySerial.write(r);
+
+    // set the appropriate LED on the strip
+    setSingleLed(r,CRGB::Blue);
+    
+    // push the pin in and out the appropriate distance
+    stepper.step(-r*30);
+    stepper.step(r*30);
+    
+    // set back to black
+    setSingleLed(r,CRGB::Black);
+    
+    mySerial.write(r*30);
   }
 }
 
-void cylon()
+void setSingleLed(byte led, byte color)
 {
-  	// First slide the led in one direction
-	for(int i = 0; i < ledCount; i++) {
-		// Set the i'th led to red 
-		leds[i] = CRGB::Red;
-		// Show the leds
-		FastLED.show();
-		// now that we've shown the leds, reset the i'th led to black
-		leds[i] = CRGB::Black;
-		// Wait a little bit before we loop around and do it again
-		delay(30);
-	}
-
-	// Now go in the other direction.  
-	for(int i = ledCount-1; i >= 0; i--) {
-		// Set the i'th led to red 
-		leds[i] = CRGB::Red;
-		// Show the leds
-		FastLED.show();
-		// now that we've shown the leds, reset the i'th led to black
-		leds[i] = CRGB::Black;
-		// Wait a little bit before we loop around and do it again
-		delay(30);
-	}
-}
-
-void myblink(byte led, byte count)
-{
-  for(byte c = 0;c<count;c++)
-  {
-    digitalWrite(led, HIGH);
-    delay(50);
-    digitalWrite(led, LOW);
-    delay(50);    
-  }
+  fill_solid(leds, ledCount, CRGB::Black);
+  leds[led] = color;
+  FastLED.show();
 }
